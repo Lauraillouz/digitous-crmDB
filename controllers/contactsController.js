645daad1d67f78ca95442d2ad709a4413919f3b3
Laura Illouz <laura.hortense@gmail.com>
2021-09-15 14:03:52 +0200

    fixed middleware + get contacts from get route OK

--- controllers/contactsController.js
+++ controllers/contactsController.js
@@ -1,6 +1,23 @@
 const Contact = require("../models/contactModel");
+const User = require("../models/userModel");
 
-const getContacts = () => {};
+const getContacts = async (req, res) => {
+  const { email } = req.body;
+
+  const user = await User.findOne({ email });
+  const userId = user._id;
+
+  const contacts = await Contact.find({ userId });
+  const numberOfContacts = contacts.length;
+
+  if (user) {
+    res.status(202).json({
+      message: "Access granted",
+      data: contacts,
+      nb: numberOfContacts,
+    });
+  }
+};
 
 const newContact = async (req, res) => {
   const contactInfo = req.body;
--- middlewares/protectData.js
+++ middlewares/protectData.js
@@ -1,21 +1,23 @@
+const User = require("../models/userModel");
+const mongoose = require("mongoose");
 const jwt = require("jsonwebtoken");
 const dotenv = require("dotenv");
 dotenv.config({
   path: "../config.env",
 });
 
-const protectData = (req, res, next) => {
-  const userId = req.body.userId;
-
+const protectData = async (req, res, next) => {
   try {
-    if (userId === data.id) {
-      const data = jwt.verify(req.cookies.jwt, process.env.JWT_SECURE);
-      req.cookies.jwtData = data;
+    const data = jwt.verify(req.cookies.jwt, process.env.JWT_SECURE);
+    req.cookies.jwtData = data;
+
+    const user = await User.findOne({ _id: data.id });
+    if (user) {
       next();
     } else {
       res.status(403).json({
         message:
-          "Access denied. You cannot add a contact for another user. Please try again with your own ID.",
+          "Access denied. You cannot access contacts of another user. Please try again with your own ID.",
       });
     }
   } catch (err) {
